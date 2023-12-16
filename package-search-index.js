dfa3ea20b5455fa9dc6592adc4196895c3b80d5b
shadowomar2 <hamshoitsolution@gmail.com>
2023-12-16 05:49:15 +0300

    Add Javadoc for version x.y.z

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.example.demo"},{"l":"com.example.demo.aspect"},{"l":"com.example.demo.configuration"},{"l":"com.example.demo.Controllers"},{"l":"com.example.demo.entities"},{"l":"com.example.demo.models"},{"l":"com.example.demo.repositories"},{"l":"com.example.demo.services"}];updateSearchResults();
